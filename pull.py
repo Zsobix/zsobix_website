63336d23444272e4639af0c6b965e19a2b37fa4c
Zsobix <index@zsobix.xyz>
2025-04-17 18:40:32 +0200

    Add files via upload

--- pull.py
+++ pull.py
@@ -1,17 +1,40 @@
-import requests
-import re
+################################################################
+#					Hungarian M3U8 playlist puller
+#				by: Zsobix (https://github.com/Zsobix)
+#		
+#						WARNING!!!!:		
+#			you will need to self host because the 
+#			idiotic mediaklikk streams are ip specific
+#
+#					i fucking hate mediaklikk
+#why does the stream need to be fucking encrypted on one stream
+#					but not on the other????
+#also the other stream can be only accessed in an iframe???????
+################################################################
+
+
+from requests import get
+import re as regex
+
+BASE_URL = "https://player.mediaklikk.hu/playernew/player.php?noflash=yes&video="
+
 m3u = []
 m3u.append('#EXTM3U x-tvg-url="https://zsobix.xyz/epg_ripper_HU1.xml"')
-urls=["mtv1live","mtv2live","mtv4live","mtv4plus","mtv5live","dunalive","os1"]
+
+urls=["mtv1live","mtv2live","mtv4live","mtv4plus","mtv5live","dunalive","hirtv"]
+
 for url in urls:
-	if url == "os1":
-		d = requests.get("https://onlinestream.live/hir-tv/videoplayer/4740-1").text.split('"')
+	if url == "hirtv":
+		request = get("https://onlinestream.live/hir-tv/videoplayer/4740-1").text.split('"')
+		# mmm yes H%C3ADr_TV
 		m3u.append('#EXTINF:-1 tvg-shift="+1" tvg-id="HIR.TV.hu" tvg-logo="https://upload.wikimedia.org/wikipedia/commons/4/44/H%C3%ADr_TV.png",HirTV')
-		for line in d:
+		for line in request:
 			if "play.m3u8" in line:
-				m3u.append(requests.get(f"https://onlinestream.live{line}").text.replace("#EXTM3U", "").replace("\n", ""))
+				# the line will start with a // or smth so i need to add the origin url
+				m3u.append(get(f"https://onlinestream.live{line}").text.replace("#EXTM3U", "").replace("\n", ""))
+	#custom logic for m4 sport (i hate you mediaklikk)
 	elif url == "mtv4live":
-		headers1 = {
+		headers = {
 			'Host': 'player.mediaklikk.hu',
 			'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:136.0) Gecko/20100101 Firefox/136.0',
 			'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
@@ -29,32 +52,39 @@ for url in urls:
 			'Pragma': 'no-cache',
 			'Cache-Control': 'no-cache'
 		}
-		r = requests.get(f'https://player.mediaklikk.hu/playernew/player.php?noflash=yes&video={url}', timeout=15, headers=headers1, cookies={"SERVERID":"playercookieA"}).text
-		ab = r.split('"')
-		for line in ab:
+		# ?????
+		# why does it need this?
+		request = get(f'{BASE_URL}{url}', timeout=15, headers=headers, cookies={"SERVERID":"playercookieA"}).text
+		lines = request.split('"')
+		for line in lines:
 			if "index.m3u8" in line:
 				m3u.append('#EXTINF:-1 tvg-id="m4.hu" tvg-logo="https://upload.wikimedia.org/wikipedia/hu/f/fd/M4_logo.png",M4 Sport')
-				line = re.sub(r'\\', "", line)
-				m3u.append(re.sub(r'\?v=5iip:((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}$', "", line))
+				# add some info
+				line = regex.sub(r'\\', "", line)
+				# https://stackoverflow.com/questions/11475885/python-replace-regex thank you stackoverflow
+				m3u.append(regex.sub(r'\?v=5iip:((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}$', "", line))
 	else:
-		r = requests.get(f'https://player.mediaklikk.hu/playernew/player.php?noflash=yes&video={url}', timeout=15).text
-		ab = r.split('"')
-		for line in ab:
+		request = get(f'{BASE_URL}{url}', timeout=15).text
+		lines = request.split('"')
+		for line in lines:
 			if "index.m3u8" in line:
-				if url == "mtv1live":
-					m3u.append('#EXTINF:-1 tvg-id="m1.HD.hu" tvg-logo="https://upload.wikimedia.org/wikipedia/commons/f/f1/M1_2020.png",M1')
-				if url == "mtv2live":
-					m3u.append('#EXTINF:-1 tvg-id="m2.HD.hu" tvg-logo="https://upload.wikimedia.org/wikipedia/hu/b/b5/M2_gyerekcsatorna_log%C3%B3ja.png",M2')
-				if url == "mtv4plus":
-					m3u.append('#EXTINF:-1 tvg-id="m4sport+.hu" tvg-logo="https://upload.wikimedia.org/wikipedia/hu/b/bd/Duna_world_log%C3%B3.png",Duna World/M4 Sport+')
-				if url == "mtv5live":
-					m3u.append('#EXTINF:-1 tvg-id="m5.hu" tvg-logo="https://upload.wikimedia.org/wikipedia/commons/b/b4/M5logo.png",M5')
-				if url == "dunalive":
-					m3u.append('#EXTINF:-1 tvg-id="Duna.TV.hu" tvg-logo="https://upload.wikimedia.org/wikipedia/hu/d/d3/Dunatv_logo.png",Duna')
-				line = re.sub(r'\\', "", line)
-				m3u.append(re.sub(r'\?v=5iip:((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}$', "", line))
+				match url:
+					# i finally added a switch statment
+					case "mtv1live":
+						m3u.append('#EXTINF:-1 tvg-id="m1.HD.hu" tvg-logo="https://upload.wikimedia.org/wikipedia/commons/f/f1/M1_2020.png",M1')
+					case "mtv2live":
+						m3u.append('#EXTINF:-1 tvg-id="m2.HD.hu" tvg-logo="https://upload.wikimedia.org/wikipedia/hu/b/b5/M2_gyerekcsatorna_log%C3%B3ja.png",M2')
+					case "mtv4plus":
+						m3u.append('#EXTINF:-1 tvg-id="m4sport+.hu" tvg-logo="https://upload.wikimedia.org/wikipedia/hu/b/bd/Duna_world_log%C3%B3.png",Duna World/M4 Sport+')
+					case "mtv5live":
+						m3u.append('#EXTINF:-1 tvg-id="m5.hu" tvg-logo="https://upload.wikimedia.org/wikipedia/commons/b/b4/M5logo.png",M5')
+					case "dunalive":
+						m3u.append('#EXTINF:-1 tvg-id="Duna.TV.hu" tvg-logo="https://upload.wikimedia.org/wikipedia/hu/d/d3/Dunatv_logo.png",Duna')
+				line = regex.sub(r'\\', "", line)
+				m3u.append(regex.sub(r'\?v=5iip:((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}$', "", line))
+# my euronews feed from youtube
 m3u.append('#EXTINF:-1 tvg-id="Euronews.hu" tvg-logo="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9c/Euronews_2022.svg/1280px-Euronews_2022.svg.png",Euronews')
 m3u.append("https://raw.githubusercontent.com/Zsobix/YouTube_to_m3u/refs/heads/main/youtube.m3u8")
-with open("hungary.m3u", "a") as hun:
-	for i in m3u:
-			hun.write(f"{i}\n")
+with open("hungary.m3u", "a") as playlist:
+	for lines in m3u:
+			playlist.write(f"{lines}\n")
